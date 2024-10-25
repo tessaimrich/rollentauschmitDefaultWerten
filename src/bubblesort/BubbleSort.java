5ef40fae6fe8563546d79aad46bc5259a239db58
rebnr57 <regina.ebner2@gmail.com>
2024-10-25 15:47:25 +0200

    Initial commit

--- src/bubblesort/BubbleSort.java
+++ src/bubblesort/BubbleSort.java
@@ -3,9 +3,11 @@ package bubblesort;
 
 public class BubbleSort {
 
+    //Main methode
     public static void main(String[] args) {
         
-        int [] array = {5, 1, 3, 8, 11};
+        int [] array = {5, 1, 3, 8, 15};
+        int [] bsp = {1, 2, 3};
         
         System.out.println("Unsortiertes array: " );
         for (int num : array) {
@@ -17,7 +19,7 @@ public class BubbleSort {
         int swapcounter = 0;
         
         do {
-            
+            System.out.println("Hallo!");
             for (int i = 0; i < array.length - 1; i++) {
                 
                 if (array[i] > array[i+1]) {
@@ -39,7 +41,8 @@ public class BubbleSort {
             
             System.out.print(num + " ");
         }
-        
+
+        //Ergebnis
         System.out.println("\nSwapcounter: " + swapcounter);
     }
     
