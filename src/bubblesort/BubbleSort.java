fa17021f0251450faf7892cd32c294adaeb969ca
tessaimrich <t.imrich@hotmail.de>
2024-10-25 16:17:46 +0200

    Initial commit 2 und 9 statt 1 und 8 kommentare hinzugefügt

--- src/bubblesort/BubbleSort.java
+++ src/bubblesort/BubbleSort.java
@@ -3,11 +3,9 @@ package bubblesort;
 
 public class BubbleSort {
 
-    //Main methode
     public static void main(String[] args) {
         
-        int [] array = {5, 1, 3, 8, 15};
-        int [] bsp = {1, 2, 3};
+        int [] array = {5, 1, 3, 8, 11};
         
         System.out.println("Unsortiertes array: " );
         for (int num : array) {
@@ -19,7 +17,7 @@ public class BubbleSort {
         int swapcounter = 0;
         
         do {
-            System.out.println("Hallo!");
+            
             for (int i = 0; i < array.length - 1; i++) {
                 
                 if (array[i] > array[i+1]) {
@@ -41,8 +39,7 @@ public class BubbleSort {
             
             System.out.print(num + " ");
         }
-
-        //Ergebnis
+        
         System.out.println("\nSwapcounter: " + swapcounter);
     }
     
